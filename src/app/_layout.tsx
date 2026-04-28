fd8463c478f12e4efd10dba1ba1f999b8f559a35
Jeff Yeung <jyeung.eng@gmail.com>
2026-04-27 20:18:49 -0700

    Add gesture handler for handling tap

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -3,17 +3,20 @@ import AppTabs from '@/components/app-tabs';
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import React from 'react';
 import { useColorScheme } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { SafeAreaProvider, initialWindowMetrics } from 'react-native-safe-area-context';
 import '../global.css';
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <SafeAreaProvider initialMetrics={initialWindowMetrics}>
-        <AnimatedSplashOverlay />
-        <AppTabs />
-      </SafeAreaProvider>
-    </ThemeProvider>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+        <SafeAreaProvider initialMetrics={initialWindowMetrics}>
+          <AnimatedSplashOverlay />
+          <AppTabs />
+        </SafeAreaProvider>
+      </ThemeProvider>
+    </GestureHandlerRootView>
   );
 }
