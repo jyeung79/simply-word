6ff0c27ead96790e0a2265d71da65a5c68273ae3
Jeff Yeung <jyeung.eng@gmail.com>
2026-04-28 22:21:34 -0700

    Fix all formatting issues across the app

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,17 +1,24 @@
-import { AnimatedSplashOverlay } from '@/components/animated-icon';
-import AppTabs from '@/components/app-tabs';
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import React from 'react';
-import { useColorScheme } from 'react-native';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import { SafeAreaProvider, initialWindowMetrics } from 'react-native-safe-area-context';
-import '../global.css';
+import { AnimatedSplashOverlay } from "@/components/animated-icon";
+import AppTabs from "@/components/app-tabs";
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import React from "react";
+import { useColorScheme } from "react-native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import {
+  SafeAreaProvider,
+  initialWindowMetrics,
+} from "react-native-safe-area-context";
+import "../global.css";
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
         <SafeAreaProvider initialMetrics={initialWindowMetrics}>
           <AnimatedSplashOverlay />
           <AppTabs />
--- src/hooks/store/useTabbar.ts
+++ src/hooks/store/useTabbar.ts
@@ -1,12 +1,12 @@
 import { create } from "zustand";
 
 interface TabbarState {
-    isMinized: boolean;
-    setIsMinized: (minimized: boolean) => void;
+  isMinized: boolean;
+  setIsMinized: (minimized: boolean) => void;
 }
 
 export const useTabbar = create<TabbarState>((set) => ({
-    isMinized: false,
-    setIsMinized: (minimized) => set({ isMinized: minimized }),
-    toggleMinized: () => set((state) => ({ isMinized: !state.isMinized })),
+  isMinized: false,
+  setIsMinized: (minimized) => set({ isMinized: minimized }),
+  toggleMinized: () => set((state) => ({ isMinized: !state.isMinized })),
 }));
